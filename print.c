2b2045fd81db68420c8f6e6dfa20e5384909214b
pinkhron <admin@pinkhron.net>
2022-09-26 18:46:47 -0700

    Initial commit

---
+++ print.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+    \n = new line
+    \t = equal spacing
+    \  = escape character
+*/
+
+int main(){
+    int num = 7;
+
+    printf("\"Hello\"\n");
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+    
+    return 0;
+}
